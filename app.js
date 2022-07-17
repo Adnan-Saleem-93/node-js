5125a05457639048acebaf5130ca49f014995fe8
Adnan Saleem <adnan13893@gmai.com>
2022-07-17 21:52:41 +0500

    variables

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+let noun1 = 'dinosaur'
+let adjective = 'silly'
+let noun2 = 'dino'
+let verb = 'laugh'
+let noun3 = 'grass'
+
+console.log(
+  `The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`
+)
