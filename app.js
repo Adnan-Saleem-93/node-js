1dcda6a080b5918c77e2bc61e02340ba348df923
Adnan Saleem <adnan13893@gmai.com>
2022-07-18 23:56:04 +0500

    require keyword

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-let noun1 = 'dinosaur'
-let adjective = 'silly'
-let noun2 = 'dino'
-let verb = 'laugh'
-let noun3 = 'grass'
+const builtinModules = require('module').builtinModules
 
-console.log(
-  `The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`
-)
+console.log(builtinModules)
