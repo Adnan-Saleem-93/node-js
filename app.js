303d3578f17cfd4252b588d5fe6849d6c0196155
Adnan Saleem <adnan13893@gmail.com>
2022-07-19 00:14:39 +0500

    utilizing process.memoryUsage

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const builtinModules = require('module').builtinModules
+let initialMemory = null
+let word = null
 
-console.log(builtinModules)
+console.log(`Your word is ${word}`)
+
+// Create a new array
+let wordArray = []
+
+// Loop 1000 times, pushing into the array each time
+for (let i = 0; i < 1000; i++) {
+  wordArray.push(`${word} count: ${i}`)
+}
+
+console.log(
+  `Starting memory usage: ${initialMemory}. \nCurrent memory usage: ${
+    process.memoryUsage().heapUsed
+  }. \nAfter using the loop to add elements to the array, the process is using ${
+    process.memoryUsage().heapUsed - initialMemory
+  } more bytes of memory.`
+)
