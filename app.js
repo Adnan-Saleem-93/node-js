5d00f43cfadfddcecde2bb31cc3d7de8c46fd769
Adnan Saleem <adnan13893@gmail.com>
2022-08-02 22:51:28 +0500

    writeable streams

--- app.js
+++ app.js
@@ -2,18 +2,16 @@ const fs = require('fs')
 const readLine = require('readline')
 
 let filePath = './files/shoppingList.txt'
+let outputFilePath = './files/shoppingResult.txt'
 
-// creating file if it doesn't exist
-if (!fs.existsSync(filePath)) {
-  let shoppingItems = ['bread', 'butter', 'milk', 'chees', 'fruits', 'vegetables']
-  fs.writeFileSync(filePath, shoppingItems.join('\n'))
+const myInterface = readLine.createInterface({
+  input: fs.createReadStream(filePath)
+})
+
+const fileStream = fs.createWriteStream(outputFilePath)
+
+const transformData = (line) => {
+  fileStream.write(`They were out of line: ${line}\n`)
 }
 
-let myInterface = readLine.createInterface({input: fs.createReadStream(filePath)})
-
-let count = 0
-let printLine = (lineData) => {
-  console.log(`Line ${++count}: ${lineData}`)
-}
-
-myInterface.on('line', printLine)
+myInterface.on('line', transformData)
