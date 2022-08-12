2dcb085021360672fb927728e8041f50d36ad2d8
Adnan Saleem <adnan13893@gmail.com>
2022-08-12 22:38:58 +0500

    mock database functionality

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 const http = require('http')
+const fs = require('fs')
+
+// GET request handler
+const handleGetRequest = (req, res) => {
+  if (req.url === '/users') {
+    // Loads the database and searches for data
+    makeDatabaseRequest('users', (err, payload) => {
+      if (err) {
+        res.writeHeader(400)
+        res.write('Error retrieving data')
+      } else {
+        // Process successful request
+        console.log(payload)
+        res.writeHeader(200, {'Content-Type': 'application/json'})
+        res.write(JSON.stringify(payload))
+      }
+      res.end()
+    })
+  }
+}
 
 // Creates server instance
 const server = http.createServer((req, res) => {
@@ -7,27 +27,23 @@ const server = http.createServer((req, res) => {
   switch (method) {
     case 'GET':
       return handleGetRequest(req, res)
-    case 'POST':
-      return handlePostRequest(req, res)
     default:
       throw new Error(`Unsupported request method: ${method}`)
   }
 })
-
-const handleGetRequest = (req, res) => {
-  // Set GET status code here
-  res.statusCode = 200
-  return res.end(JSON.stringify({data: []}))
-}
-
-const handlePostRequest = (req, res) => {
-  // Set POST status code here
-  res.statusCode = 500
-  return res.end('Unable to create record')
-}
 
 // Starts server listening on specified port
 server.listen(4001, () => {
   const {address, port} = server.address()
   console.log(`Server is listening on: http://${address}:${port}`)
 })
+
+function makeDatabaseRequest(type, cb) {
+  fs.readFile('./database.json', 'utf8', function (err, payload) {
+    if (err) {
+      cb(err, null)
+    } else {
+      cb(null, JSON.parse(payload)[type])
+    }
+  })
+}
